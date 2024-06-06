56e889965d6cf3c914a25d63b150f74b813cecef
ApoorvCeletel <apoorv@celetel.com>
2024-06-06 09:03:24 +0530

    logout

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Container, Row, Col, Table, Button, Spinner, Alert } from 'react-bootstrap';
 
 const NotesList = () => {
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -23,20 +24,21 @@ const NotesList = () => {
   }, []);
 
   const handleDeleteNote = async (id) => {
-    // Ask for confirmation
     const confirmDelete = window.confirm('Are you sure you want to delete this note?');
-    if (!confirmDelete) return; // If user cancels deletion, exit function
-    
+    if (!confirmDelete) return;
+
     try {
-      // Make DELETE request to delete the note
       await axios.delete(`https://backendassign-59wq.onrender.com/api/notes/deleteNote/${id}`);
-      
-      // Remove the deleted note from the local state
       setNotes(notes.filter(note => note._id !== id));
     } catch (error) {
       setError('Error deleting the note');
       console.error(error);
     }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    navigate('/login');
   };
 
   if (loading) {
@@ -60,8 +62,11 @@ const NotesList = () => {
       <Row className="mb-4">
         <Col>
           <h2>Notes</h2>
-          <Button as={Link} to="/notes" variant="primary">
+          <Button as={Link} to="/notes" variant="primary" className="me-2">
             Create Note
+          </Button>
+          <Button onClick={handleLogout} variant="secondary">
+            Logout
           </Button>
         </Col>
       </Row>
@@ -83,13 +88,12 @@ const NotesList = () => {
                   <td>{note.title}</td>
                   <td>{note.description}</td>
                   <td>
-                  <Button onClick={() => handleDeleteNote(note._id)} variant="outline-primary" size="sm" className="me-2">
+                    <Button onClick={() => handleDeleteNote(note._id)} variant="outline-primary" size="sm" className="me-2">
                       Delete
                     </Button>
                     <Button as={Link} to={`/notes/${note._id}/edit`} variant="outline-secondary" size="sm">
                       Edit
                     </Button>
-
                   </td>
                 </tr>
               ))}
